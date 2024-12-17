ef485aa1b98da208f6156dd9f58b7dce85e69878
Jelle Adyns <137185777+JelleAdyns@users.noreply.github.com>
2024-12-17 19:55:21 +0100

    Removed targetver.h

--- framework.h
+++ framework.h
@@ -1,7 +1,6 @@
 #ifndef FRAMEWORK_H
 #define FRAMEWORK_H
 
-#include "targetver.h"
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
