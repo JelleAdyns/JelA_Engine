092c126977105a6f1f243bbf31311d1299b6f186
Jelle Adyns <137185777+JelleAdyns@users.noreply.github.com>
2026-09-03 20:36:26 +0200

    Removed ComponentIndex

--- Engine/src/ObservingObjects.cpp
+++ Engine/src/ObservingObjects.cpp
@@ -1,22 +1,6 @@
 #include "ObservingObjects.h"
-#include "ComponentManager.h"
 
 namespace jela
 {
-    void ComponentIndex::Notify(const CompsChangedInfo& changeInfo)
-    {
-        if (!GetValue().has_value()) return;
 
-        if (GetValue() == changeInfo.removedComp)
-        {
-            ResetValue();
-            return;
-        }
-
-        if (changeInfo.changedIndexInfo.has_value())
-        {
-            const auto [prevIndex, newIndex] = changeInfo.changedIndexInfo.value();
-            if (GetValue() == prevIndex) SetValue(newIndex);
-        }
-    }
 }
